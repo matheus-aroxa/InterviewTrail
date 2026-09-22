c498a8e5a03beafc2e5ca55f5aa6dc0588fbe55c
matheut <matheus.teixeira@tjpe.jus.br>
2026-09-22 09:58:01 -0300

    feat: initial setup

---
+++ backend/src/main/java/com/interviewtrail/InterviewTrail/InterviewTrailApplication.java
@@ -0,0 +1,12 @@
+package com.interviewtrail.InterviewTrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterviewTrailApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/com/interviewtrail/InterviewTrail/InterviewTrailApplicationTests.java
@@ -0,0 +1,11 @@
+package com.interviewtrail.InterviewTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrailApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
